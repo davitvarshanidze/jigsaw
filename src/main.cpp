ce3e19d9aeba949d35a3548802135c0c40d42825
davitvarshanidze <davittvarshanidze@gmail.com>
2025-12-11 01:41:39 +0400

    format

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@ static GLFWwindow* init(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
 #if __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
