e219b616804bd971765a973db3f8250309ad85cd
davitvarshanidze <davittvarshanidze@gmail.com>
2025-12-11 01:21:58 +0400

    image loaded correctly via stb_image.h + updated shaders to match data

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ static GLFWwindow* init(void)
         fprintf(stderr, "GLFW initialization failed\n");
         return NULL;
     }
-    
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -32,13 +32,13 @@ static GLFWwindow* init(void)
 #endif
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Renderer", NULL, NULL);
-	glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, key_callback);
     if (!window) {
         fprintf(stderr, "GLFW window creation failed\n");
         glfwTerminate();
         return NULL;
     }
-    
+
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
@@ -56,71 +56,108 @@ static GLFWwindow* init(void)
 int main(UNUSED int argc, UNUSED char **argv)
 {
     GLFWwindow* window = init();
-    if (!window) {
-        return EXIT_FAILURE;
-    }
+    if (!window) return EXIT_FAILURE;
 
-    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	float points[] = {
-		0.0f,  0.5f,  0.0f, // x,y,z of first point.
-		0.5f, -0.5f,  0.0f, // x,y,z of second point.
-		-0.5f, -0.5f,  0.0f  // x,y,z of third point.
-	};
+    int w, h, ch;
+    stbi_set_flip_vertically_on_load(1);
+    unsigned char* data = stbi_load("kawai.jpg", &w, &h, &ch, 4);
+    if (!data) {
+        printf("Failed to load image\n");
+        return 0;
+    }
 
-	GLuint vbo = 0;
-	glGenBuffers( 1, &vbo );
-	glBindBuffer( GL_ARRAY_BUFFER, vbo );
-	glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( float ), points, GL_STATIC_DRAW );
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
 
-	GLuint vao = 0;
-	glGenVertexArrays( 1, &vao );
-	glBindVertexArray( vao );
-	glEnableVertexAttribArray( 0 );
-	glBindBuffer( GL_ARRAY_BUFFER, vbo );
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, data);
 
-	// Vertex/Fragment shader sample
-	const char* vertex_shader =
-		"#version 410 core\n"
-		"layout(location = 0) in vec3 vp;"
-		"void main() {"
-		"  gl_Position = vec4( vp, 1.0 );"
-		"}";
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	// VAO (vertex array object), we need to save vertex data to then feed it to shaders
-	const char* fragment_shader =
-		"#version 410 core\n"
-		"out vec4 frag_colour;"
-		"void main() {"
-		"  frag_colour = vec4( 0.5, 0.0, 0.5, 1.0 );"
-		"}";
+    stbi_image_free(data);
 
-	GLuint vs = glCreateShader( GL_VERTEX_SHADER );
-	glShaderSource( vs, 1, &vertex_shader, NULL );
-	glCompileShader( vs );
-	GLuint fs = glCreateShader( GL_FRAGMENT_SHADER );
-	glShaderSource( fs, 1, &fragment_shader, NULL );
-	glCompileShader( fs );
+    float quad[] = {
+        -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.0f, 1.0f
+    };
 
-	GLuint shader_program = glCreateProgram();
-	glAttachShader( shader_program, fs );
-	glAttachShader( shader_program, vs );
-	glLinkProgram( shader_program );
-	
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+    unsigned int idx[] = {0,1,2, 2,3,0};
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-		glUseProgram(shader_program);
-		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
 
-		glfwSwapBuffers(window);
-	}
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)(2 * sizeof(float)));
+
+    const char* vs_src =
+        "#version 410 core\n"
+        "layout(location = 0) in vec2 pos;\n"
+        "layout(location = 1) in vec2 uv;\n"
+        "out vec2 v_uv;\n"
+        "void main(){\n"
+        "    gl_Position = vec4(pos, 0.0, 1.0);\n"
+        "    v_uv = uv;\n"
+        "}\n";
+
+    const char* fs_src =
+        "#version 410 core\n"
+        "in vec2 v_uv;\n"
+        "out vec4 frag;\n"
+        "uniform sampler2D tex0;\n"
+        "void main(){\n"
+        "    frag = texture(tex0, v_uv);\n"
+        "}\n";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vs_src, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fs_src, NULL);
+    glCompileShader(fs);
+
+    GLuint shader = glCreateProgram();
+    glAttachShader(shader, vs);
+    glAttachShader(shader, fs);
+    glLinkProgram(shader);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+	// @main-loop
+    while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(vao);
+        glBindTexture(GL_TEXTURE_2D, tex);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     glfwTerminate();
-    return EXIT_SUCCESS;
+    return 0;
 }
